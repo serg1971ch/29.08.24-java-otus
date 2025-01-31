c4b94cf2031ee56ba06889600517289be92992f2
serg1971ch <serg1971ch@yandex.ru>
2025-02-01 02:43:17 +0300

    добавил код с рассчетами

--- hw10-webServer/src/main/java/ru/otus/WebCalc.java
+++ hw10-webServer/src/main/java/ru/otus/WebCalc.java
@@ -14,16 +14,30 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "WebCalc", urlPatterns = "/calc")
 public class WebCalc extends HttpServlet {
-    private static Logger logger = LoggerFactory.getLogger(WebCalc.class);
+    private static final Logger logger = LoggerFactory.getLogger(WebCalc.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
         resp.setContentType("text/html");
-        int min = Integer.parseInt(req.getParameter("min"));
-        int max = Integer.parseInt(req.getParameter("max"));
-        String result = String.valueOf(min + (int) (Math.random() * (max - min)));
-        out.println("<html><body><h1>" + result + "</h1></body></html>");
+        int a = Integer.parseInt(req.getParameter("a"));
+        int b = Integer.parseInt(req.getParameter("b"));
+        String operator = String.valueOf(req.getParameter("op"));
+
+        switch (operator) {
+            case "+":
+                String plusResult = String.valueOf(a + b);
+                out.println("<html><body>" + plusResult + "</body></html>");
+            case "-":
+                String minusResult = String.valueOf(a - b);
+                out.println("<html><body>" + minusResult + "</body></html>");
+            case "*":
+                String multiplyResult = String.valueOf(a * b);
+                out.println("<html><body>" + multiplyResult + "</body></html>");
+            case "/":
+                String divideResult = String.valueOf(a / b);
+                out.println("<html><body>" + divideResult + "</body></html>");
+        }
         out.close();
     }
 }
